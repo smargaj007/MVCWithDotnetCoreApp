4c13ea8f9a8ac2e6e1781158efff7c32cd3b1f08
smargaj007 <smargaj007@gmail.com>
2020-06-28 21:30:07 +0530

    New MVC projects added

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCWithDotnetCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
